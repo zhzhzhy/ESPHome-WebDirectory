43eb7c2c01b39b9ace570c349a7ba0fcb755e0df
geek <by6dx1@gmail.com>
2022-09-15 21:49:50 +0800

    eventsource api

--- js/index.js
+++ js/index.js
@@ -1,21 +1,40 @@
 
     async function doRequest() {
-        let url = 'http://10.0.0.127';
+        let url = 'http://10.0.0.127/events';
         let res = await fetch(url);
 
         if (res.ok) {
 
             let text = await res.text();
-
+            console.log(text);
             return text;
         } else {
             return `HTTP error: ${res.status}`;
         }
     }
 
-    doRequest().then(data => {
-        console.log(data);
-        document.getElementById("test_url").innerHTML += data;
-    });
+function htmlToText(html) {
+    var temp = document.createElement('div');
+    temp.innerHTML = html;
+    return temp.textContent; // Or return temp.innerText if you need to return only visible text. It's slower.
+}
 
+   // doRequest().then(data => {
+   //     console.log(data);
+  //      document.getElementById("test_url").innerHTML += data.text;
+  //  });
+
+
+    
+const evtSource = new EventSource('http://10.0.0.127/events');
+const eventList = document.querySelector('ul');
+
+evtSource.onmessage = (e) => {
+  const newElement = document.createElement("li");
+  
+  newElement.textContent = `message: ${e.data}`;
+  eventList.appendChild(newElement);
+  document.getElementById("test_url").innerHTML = e.data;
+  console.log(e.data);
+}
 

5eeb40d2a4ef783bbd92946e2036bdb81f5b489a
geek <by6dx1@gmail.com>
2022-09-18 15:06:06 +0800

    eventsource res ok

--- js/index.js
+++ js/index.js
@@ -1,38 +1,46 @@
 import fetch from 'node-fetch';
 import EventSource from 'eventsource';
-//const EventSource = require('eventsource');
 
-    async function doRequest() {
-        let url = 'http://10.0.0.127/events';
-        //let res = await fetch(url);
-        let res = await fetch(url);
-        console.log(res);
-        let text = `${res.statusText} ${res.status}`;
-            //let text = res.text();
-            //console.log(text);
-            return text;
-       
-    }
+//    async function doRequest() {
+//        let url = 'http://10.0.0.127/events';
+//        //let res = await fetch(url);
+//        let res = await fetch(url);
+//        console.log(res);
+//        let text = `${res.statusText} ${res.status}`;
+//            //let text = res.text();
+//            //console.log(text);
+//            return text;
+//       
+//    }
 
-//function htmlToText(html) {
-//    var temp = document.createElement('div');
-//    temp.innerHTML = html;
-//    return temp.textContent; // Or return temp.innerText if you need to return only visible text. It's slower.
-//}
 
-    doRequest().then(data => {
-        //let a = data.toString();
-        console.log(data);
-    });
+   // doRequest().then(data => {
+   //     //let a = data.toString();
+   //     console.log(data);
+   // });
 
 
     
-const evtSource = new EventSource('http://10.0.0.127/events');
-//const eventList = document.querySelector('ul');
+const evtSource = new EventSource('http://10.0.0.190/events');
 
-evtSource.onmessage = (e) => {
-  
-  newElement.textContent = `message: ${e.data}`;
-  console.log(e.data);
-}
+evtSource.addEventListener('open', function(e) {
+    console.log("Events Connected");
+  }, false);
+
+evtSource.addEventListener('error', function(e) {
+    if (e.target.readyState != EventSource.OPEN) {
+      console.log("Events Disconnected");
+    }
+  }, false);
+
+evtSource.addEventListener('ping', function(e) {
+    console.log("ping", e.data);
+  }, false);
+
+evtSource.addEventListener('state', function(e) {
+    console.log("state", e.data);
+  }, false);
+  evtSource.addEventListener('log', function(e) {
+    console.log("log", e.data);
+  }, false);
 

82122c0f476850e9f9057e9a8639ee1dfe51ca77
geek <by6dx1@gmail.com>
2022-09-18 16:06:28 +0800

    Export EventHandler as ES6 module

---
+++ app.js
@@ -0,0 +1,5 @@
+//let a = require("./js/eventhandler.js")
+import EventHandler from './js/eventhandler.mjs'
+
+EventHandler("10.0.0.190","state");
+//console.log(EventHandler("10.0.0.190","state"));
--- js/eventhandler.mjs
+++ js/eventhandler.mjs
@@ -1,5 +1,6 @@
 import EventSource from 'eventsource';
 
+// async function: Abandoned
 //    async function doRequest() {
 //        let url = 'http://10.0.0.127/events';
 //        //let res = await fetch(url);
@@ -18,9 +19,14 @@ import EventSource from 'eventsource';
    //     console.log(data);
    // });
 
+// Eventhandler for ESPHome Eventsource API
+// Example:: 
+// EvenHandler("10.0.0.190","state");
 
+
+export default function EvenHandler(addr,types) {
     
-const evtSource = new EventSource('http://10.0.0.190/events');
+const evtSource = new EventSource('http://' + addr + '/events');
 
 evtSource.addEventListener('open', function(e) {
     console.log("Events Connected");
@@ -32,17 +38,24 @@ evtSource.addEventListener('error', function(e) {
     }
   }, false);
 
+if (types === 'ping') {
 evtSource.addEventListener('ping', function(e) {
     console.log("ping", e.data);
   }, false);
+}
 
+if (types === 'state') {
 evtSource.addEventListener('state', function(e) {
     console.log("state", e.data);
     //let a = JSON.stringify(e.data);
     let a = JSON.parse(e.data);
     console.log(a.id);
   }, false);
+}
+
+if (types === 'log') {
   evtSource.addEventListener('log', function(e) {
     console.log("log", e.data);
   }, false);
-
+}
+}

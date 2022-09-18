62cc0973399946e091b933317e3faefa90032e41
geek <by6dx1@gmail.com>
2022-09-18 11:37:13 +0800

    nodejs => index

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
+import fetch from 'node-fetch';
+import EventSource from 'eventsource';
+//const EventSource = require('eventsource');
 
     async function doRequest() {
-        let url = 'http://10.0.0.127/events';
+        let url = 'http://10.0.0.127';
         let res = await fetch(url);
 
         if (res.ok) {
@@ -13,28 +16,24 @@
         }
     }
 
-function htmlToText(html) {
-    var temp = document.createElement('div');
-    temp.innerHTML = html;
-    return temp.textContent; // Or return temp.innerText if you need to return only visible text. It's slower.
-}
+//function htmlToText(html) {
+//    var temp = document.createElement('div');
+//    temp.innerHTML = html;
+//    return temp.textContent; // Or return temp.innerText if you need to return only visible text. It's slower.
+//}
 
-   // doRequest().then(data => {
-   //     console.log(data);
-  //      document.getElementById("test_url").innerHTML += data.text;
-  //  });
+    doRequest().then(data => {
+        console.log(data);
+    });
 
 
     
 const evtSource = new EventSource('http://10.0.0.127/events');
-const eventList = document.querySelector('ul');
+//const eventList = document.querySelector('ul');
 
 evtSource.onmessage = (e) => {
-  const newElement = document.createElement("li");
   
   newElement.textContent = `message: ${e.data}`;
-  eventList.appendChild(newElement);
-  document.getElementById("test_url").innerHTML = e.data;
   console.log(e.data);
 }
 

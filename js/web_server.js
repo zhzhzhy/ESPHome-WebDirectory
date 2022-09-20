a33f589af9bba2200fd92254c098a83e05f06588
geek <by6dx1@gmail.com>
2022-09-20 23:38:17 +0800

    fix promise read file not return

--- js/web_server.js
+++ js/web_server.js
@@ -1,27 +1,18 @@
 import http from 'http'
 import  fs, { read } from 'fs'
 import { promises as fas } from 'fs'
+import { resolve } from 'path';
+import { rejects } from 'assert';
 
 const server = http.createServer( (req,res) =>{
     console.log("Web server hello!");
-    //const data = read_file("./index.html",res);
-    //console.log(data);
-    //res.write("hsjhfj",'utf8',(res) => {read_file("./index.html",res)});
-    //res.write("tesjfhgjs");
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World!');
-    const data = read_file("./index.html");
-    console.log(data);
-    res.end();
-
+    read_file("./index.html").then(result => {res.write(result);res.end();}).catch(() => {console.log("No such file!");res.end();});
+    //res.end();
 }).listen(8080)
 
-async function read_file(path){
-    const result = await fs.readFile(path,'utf-8',(err,data) => {
-        if(err){throw err;return}
-        //console.log(data);
-        console.log(result);
-        //res.write('Hello  World!');
-        return result;
-    })
+function read_file(path){
+    return new Promise((resolve,reject) => {fs.readFile(path,'utf-8',(err,data) => {
+        if(err){reject();return}
+        resolve(data);
+    })})
 }

3f5ca6ae913dfe2095fdfa21d951a5de1e21557e
geek <by6dx1@gmail.com>
2022-09-24 20:26:47 +0800

    test index.html

---
+++ asset/script.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+let Input_IP;
+function onload(){
+    Input_IP = document.getElementById(IP_address);
+}
+function alert_ip() {
+    alert(Input_IP.value);
+}
+
+
+
+</script>

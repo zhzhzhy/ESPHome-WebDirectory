79098e6253c8c8e3d0e0bfdeb8cc52edf62dfd76
geek <by6dx1@gmail.com>
2022-09-26 20:29:27 +0800

    do with req

--- js/web_server.js
+++ js/web_server.js
@@ -3,11 +3,18 @@ import  fs, { read } from 'fs'
 import { promises as fas } from 'fs'
 import { resolve } from 'path';
 import { rejects } from 'assert';
+//const util = require('node:util');
 
 const server = http.createServer( (req,res) =>{
-    console.log("Web server hello!");
-    read_file("./index.html").then(result => {res.write(result);res.end();}).catch(() => {console.log("No such file!");res.end();});
-    //res.end();
+    const method = req.method;
+    console.log("method: ",method);
+    const url = req.url;
+    console.log("url: ",url);
+    req.query = new URLSearchParams(url.split('?')[1]);
+    const request_json = JSON.stringify(Object.fromEntries(req.query.entries()));
+    //const entries = util.inspect(req.query.entries);
+    console.log("request_json:",request_json);
+    read_file("./index.html").then(result => {res.write(result);res.end(request_json);}).catch(() => {console.log("No such file!");res.end();});
 }).listen(8080)
 
 function read_file(path){

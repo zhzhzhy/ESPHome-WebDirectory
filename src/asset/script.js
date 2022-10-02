18343e9c300decfb46b2ab2efc7d07fea38b0782
geek <by6dx1@gmail.com>
2022-10-03 00:27:51 +0800

    remove component logic

--- src/asset/script.js
+++ src/asset/script.js
@@ -1,6 +1,5 @@
 function Handle_Input_IP(callback) {
     let Input_IP;
-    $(document).ready(function(){
       $("#Input_IP").click(function(){
     Input_IP =  document.getElementById('IP_address');
     if(!Input_IP?.value){
@@ -10,11 +9,15 @@ function Handle_Input_IP(callback) {
     callback(Input_IP?.value);
     }
   });
-  })
+  
 }
 
 function Add_Component(data){
 
-      $("ul.component").append("<li>" + data + "</li>");
+      $("ul.component").append("<li class='component'>" + data + "</li>");
 
+}
+
+function Remove_Component(){
+$("ul.component").on('dblclick',"li",function() {console.log("Remove component: ",$(this).text());$(this).remove();})
 }

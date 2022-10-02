a875a61c83426cdcbac423cc3ea42dd80a2286a5
geek <by6dx1@gmail.com>
2022-10-02 23:04:43 +0800

    jquery add_IP_addr

--- src/asset/script.js
+++ src/asset/script.js
@@ -1,8 +1,20 @@
-function Handle_Input_IP() {
+function Handle_Input_IP(callback) {
     let Input_IP;
+    $(document).ready(function(){
+      $("#Input_IP").click(function(){
     Input_IP =  document.getElementById('IP_address');
     if(!Input_IP?.value){
       alert("Empty IP address!");
-    } 
+    }else{ 
     console.log(Input_IP.value);
+    callback(Input_IP?.value);
+    }
+  });
+  })
+}
+
+function Add_Component(data){
+
+      $("ul.component").append("<li>" + data + "</li>");
+
 }

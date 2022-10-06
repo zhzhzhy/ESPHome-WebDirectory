7959fab9139b1dd600abb0f81d43430c48fe4bb7
geek <by6dx1@gmail.com>
2022-10-07 00:02:53 +0800

    add ip addr card

--- src/asset/script.js
+++ src/asset/script.js
@@ -23,12 +23,17 @@ function Handle_Input_IP(callback) {
   
 }
 
-function Add_Component(data){
+function Add_Component(data,Jquery_Object){
       const ip_regex = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|$)){4}$/;
       if((typeof data === 'string' || data instanceof String) && ip_regex.test(data)){
-      const Data_Append = $("<li/>").attr("id",data).addClass("component list-group-item row").html(`${data}`);
-      //console.log(data_append);
-      $("ul.component").append(Data_Append);
+      //const Data_Append = $("<div/>").attr("id",data).addClass("component row ").html(`${data}`);
+      //let Data_Append = Jquery_Object.removeAttr('id').children("h5.card-title").html(`${data}`);
+      let Data_Append = Jquery_Object.attr("id",data);
+      // console.log(Data_Append);
+      $("#device_list").prepend(Data_Append);
+      const id = data.replace(/\./g,"\\.");
+      const selector = '#' + id + ' ' + 'h5.card-title';
+      $(selector).html(`${data}`);
       }else{
           alert(`IP address invalid: ${data}`);
       }
@@ -36,6 +41,6 @@ function Add_Component(data){
 
 function Remove_Component(){
   
-      $("ul.component").on('dblclick',"li",function() {console.log("Remove component: ",$(this).text());$(this).remove();})
+      $("ul.component").on('dblclick',"div.component",function() {console.log("Remove component: ",$(this).text());$(this).remove();})
 
 }

93e38a1a08c9b724e432aeab96759f3ad6f8fc69
geek <by6dx1@gmail.com>
2022-10-09 22:27:26 +0800

    server side event

--- src/js_node/app.js
+++ src/js_node/app.js
@@ -1,5 +1,5 @@
 import EventHandler from './eventhandler.js'
 
 //EventHandler("10.0.0.218","state",(a) => {console.log("test pass!",a)});
-EventHandler("10.0.0.190","log",(a) => {console.log("test pass!",a)});
+EventHandler("10.0.0.190","state",(a) => {console.log("test pass!",a)});
 //console.log(EventHandler("10.0.0.190","state"));

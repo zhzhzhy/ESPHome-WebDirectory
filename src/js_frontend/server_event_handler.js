d9c83123e61d7e35a840f7908323caf5d8cf87c4
geek <by6dx1@gmail.com>
2022-10-23 22:53:46 +0800

    JSON parse component id

---
+++ src/js_frontend/server_event_handler.js
@@ -0,0 +1,38 @@
+function Parse_Server_Event(id,data,callback) {
+    
+}
+
+function Update_Tree_Data(id,data,callback) {
+    
+}
+
+function Append_Tree_Template(id,data,callback){
+    //let component_id_group = new Set();
+    //console.log(JSON.parse(data));
+    let data_obj = {};
+    data_obj = JSON.parse(data);
+    console.log(data_obj.id);
+    let c_id = data_obj.id.split('-');
+    let c_value = data_obj.value;
+    let c_name = data_obj.name;
+    component_id_group.add(c_id[1]);
+    console.log(component_id_group);
+    let template_tree = ``;
+
+}
+
+function Remove_Tree_Template(id,data,callback){
+    
+}
+
+function Operate_Tree_data(id,data,operation,callback){
+    if(operation === "add"){
+        Append_Tree_Template();
+    }
+    if(operation === "remove"){
+
+    }
+    if(operation === "update"){
+
+    }
+}

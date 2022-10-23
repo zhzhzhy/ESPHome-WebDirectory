0a5aef07a1180441c0620394f4c22462e96bc56d
geek <by6dx1@gmail.com>
2022-10-23 21:32:10 +0800

    change to include socket.io without ES module

--- src/js_frontend/main.js
+++ src/js_frontend/main.js
@@ -1,9 +1,5 @@
 import { io } from "../asset/socket.io/socket.io.esm.min.js";
 import { Parse_Socket_io } from "./handle_socket_io.js";
 
-let Addr_Group = new Set();
 
-
-
-Parse_Socket_io();
-console.log(Addr_Group);
+//window.Parse_Socket_io = Parse_Socket_io;

835302cd6876794b171bdfe63eb8600037bcdc5d
geek <by6dx1@gmail.com>
2022-10-24 22:14:43 +0800

    edit  comments

--- src/js_frontend/server_event_handler.js
+++ src/js_frontend/server_event_handler.js
@@ -3,11 +3,9 @@ Parse socket.io eventsource data to Set & Map for template usage
 Set:Component_ID_Group(Set include data--id) & Map:Component_ID_Map(Map like: data--id => data--object)
 */
 function Parse_Server_Event(id,data,callback) {
-    //let Component_ID_Group = new Set();
-    //console.log(JSON.parse(data));
-    //let data_obj = {};
+    // Component_ID_Group -> Object.Set is define global scope or in closer
+    // Component_ID_Map -> Object.Map is define global scope or in closer
     let data_obj = JSON.parse(data); //component data object(use this as template raw data)
-    console.log(data_obj.id);
     let c_id = data_obj.id; //component object id(use this to determine unique id)
     let c_value = data_obj.value; // not necessary
     let c_name = data_obj.name; // not necessary
@@ -17,6 +15,7 @@ function Parse_Server_Event(id,data,callback) {
     if (!Component_ID_Map.has(c_id)) {
         Component_ID_Map.set(c_id,data_obj);
     }
+    console.log(data_obj.id);  //comment later!
     console.log(Component_ID_Group); //comment later!
     console.log(Component_ID_Map); //comment later!
     callback(Component_ID_Group,Component_ID_Map);

6dd3e3f618a32e366d0d2f1c10b9016acefe9864
geek <by6dx1@gmail.com>
2022-10-24 21:52:20 +0800

    new map: id => component_data_obj

--- src/js_frontend/server_event_handler.js
+++ src/js_frontend/server_event_handler.js
@@ -1,5 +1,20 @@
 function Parse_Server_Event(id,data,callback) {
-    
+    //let Component_ID_Group = new Set();
+    //console.log(JSON.parse(data));
+    //let data_obj = {};
+    let data_obj = JSON.parse(data); //component data object(use this as template raw data)
+    console.log(data_obj.id);
+    let c_id = data_obj.id; //component object id(use this to determine unique id)
+    let c_value = data_obj.value; // not necessary
+    let c_name = data_obj.name; // not necessary
+    if (!Component_ID_Group.has(c_id)) {
+        Component_ID_Group.add(c_id);
+    }
+    if (!Component_ID_Map.has(c_id)) {
+        Component_ID_Map.set(c_id,data_obj);
+    }
+    console.log(Component_ID_Group);
+    console.log(Component_ID_Map);
 }
 
 function Update_Tree_Data(id,data,callback) {
@@ -7,17 +22,8 @@ function Update_Tree_Data(id,data,callback) {
 }
 
 function Append_Tree_Template(id,data,callback){
-    //let component_id_group = new Set();
-    //console.log(JSON.parse(data));
-    //let data_obj = {};
-    let data_obj = JSON.parse(data);
-    console.log(data_obj.id);
-    let c_id = data_obj.id;
-    let c_value = data_obj.value;
-    let c_name = data_obj.name;
-    component_id_group.add(c_id);
-    console.log(component_id_group);
-    let template_tree = ``;
+   
+    //let template_tree = ``; //write HTML template fro bootstrap tree-view
 
 }
 

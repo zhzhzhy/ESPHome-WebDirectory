5f5eab1c3c882b270716770468a3e05921f57c58
geek <by6dx1@gmail.com>
2022-11-02 23:44:23 +0800

    comment

--- src/js_frontend/script.js
+++ src/js_frontend/script.js
@@ -156,9 +156,9 @@ function Create_Tree_Element(group_map,callback) {
   //console.log(all_node.querySelector(".component_list"));
   //console.log(node);
   for(i of group_map.entries()){
-    console.log(i);
+    //console.log(i);
     node.textContent = i[0];
-    console.log(i[1]);
+    //console.log(i[1]);
     const component_group = i[1].Component_ID_Group;
     const comment_map = i[1].Component_ID_Map;
     component_group.forEach(element => {

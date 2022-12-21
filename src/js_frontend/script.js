4460ad6097072dcda6bdd3cb8fe262b8607dcd40
geek <by6dx1@gmail.com>
2022-12-21 21:35:48 +0800

    remove unused

--- src/js_frontend/script.js
+++ src/js_frontend/script.js
@@ -19,28 +19,28 @@ const alertBS = (message, type, alertPlaceholder) => {
 
   alertPlaceholder.after(wrapper);
 
-   setTimeout(() => {
+  setTimeout(() => {
     const alert = bootstrap.Alert.getOrCreateInstance('.alert');
     alert.close();
   }, 4000);
 
 }
 
-/* 
-*append template HTML in the text/template section of <script> tag
-*/
-function appendTemplate(selector, items) {
-  var itemTpl = $('script[data-template="template_card"]').text().split(/\$\{(.+?)\}/g);
-  function render(props) {
-    return function (tok, i) {
-      return (i % 2) ? props[tok] : tok;
-    };
-  }
-  let aped = items.map(function (item) {
-    return itemTpl.map(render(item)).join('');
-  });
-  selector.prepend(aped);
-}
+// /* 
+// *append template HTML in the text/template section of <script> tag
+// */
+// function appendTemplate(selector, items) {
+//   var itemTpl = $('script[data-template="template_card"]').text().split(/\$\{(.+?)\}/g);
+//   function render(props) {
+//     return function (tok, i) {
+//       return (i % 2) ? props[tok] : tok;
+//     };
+//   }
+//   let aped = items.map(function (item) {
+//     return itemTpl.map(render(item)).join('');
+//   });
+//   selector.prepend(aped);
+// }
 
 // handle IP input to addComponentCard
 function handleInputIP(callback) {
@@ -48,39 +48,44 @@ function handleInputIP(callback) {
   function handleEvent() {
     inputIP = document.getElementById('ipAddress');
     if (!inputIP?.value) {
-      alertBS("Empty IP address!", "danger", $("#IP_input_box"));
+      alertBS("Empty IP address!", "danger", document.getElementById("IP_input_box"));
     } else {
       console.log("IP addr & component added: ", inputIP.value);
       callback(inputIP?.value);
     }
   }
 
-  $("#basic-Commit-IP").click(function () {
+  document.getElementById("basic-Commit-IP").addEventListener("click", function () {
     handleEvent();
-  });
+  })
 
-  $("#ipAddress").on('keyup', function (e) {
+  document.getElementById("ipAddress").addEventListener("keypress", (e) => {
     if (e.key === 'Enter' || e.keyCode === 13) {
       handleEvent();
-
     }
   });
+  // $("#ipAddress").on('keyup', function (e) {
+  //   if (e.key === 'Enter' || e.keyCode === 13) {
+  //     handleEvent();
+
+  //   }
+  // });
 
 }
 
-//Old addComponent function deprecated 
-function addComponent(data, jqueryObject) {
-  const ipRegex = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|$)){4}$/;
-  if ((typeof data === 'string' || data instanceof String) && ipRegex.test(data)) {
-    let dataAppend = jqueryObject.attr("id", data);
-    $("#device_list").prepend(dataAppend);
-    const id = data.replace(/\./g, "\\.");
-    const selectorH5 = '#' + id + ' ' + 'h5.card-title';
-    $(selectorH5).html(`${data}`);
-  } else {
-    alertBS(`IP address invalid: ${data}`, "danger", $("#IP_input_box"));
-  }
-}
+// //Old addComponent function deprecated 
+// function addComponent(data, jqueryObject) {
+//   const ipRegex = /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|$)){4}$/;
+//   if ((typeof data === 'string' || data instanceof String) && ipRegex.test(data)) {
+//     let dataAppend = jqueryObject.attr("id", data);
+//     document.getElementById("device_list").prepend(dataAppend);
+//     const id = data.replace(/\./g, "\\.");
+//     const selectorH5 = '#' + id + ' ' + 'h5.card-title';
+//     $(selectorH5).html(`${data}`);
+//   } else {
+//     alertBS(`IP address invalid: ${data}`, "danger",document.getElementById("IP_input_box"));
+//   }
+// }
 
 /*
 *Add Component_Card :
@@ -100,7 +105,7 @@ function addComponentCard(data, selector) {
     }];
     appendTemplate(selector, items);
   } else {
-    alertBS(`IP address invalid: ${data}`, "danger", $("#IP_input_box"));
+    alertBS(`IP address invalid: ${data}`, "danger", document.getElementById("IP_input_box"));
   }
 }
 
@@ -124,7 +129,7 @@ function maintainAddrGroup(params, Operation, callback) {
         console.log("New components group set: ", [...addrGroup].map(data => `\[${data}\]`).join(","));
         callback();
       } else {
-        alertBS(`Duplicated IP address: ${params}`, "danger", $("#IP_input_box"));
+        alertBS(`Duplicated IP address: ${params}`, "danger", document.getElementById("IP_input_box"));
       }
     }
     if (Operation === "delete" | Operation === "remove") {
